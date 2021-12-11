2824ccd8b7e42a9b67dd26309cb481194dd409c6
petermordaunt <peetfire@hotmail.com>
2021-12-11 14:37:21 +0000

    controller file structure created

---
+++ code/controllers/owners_controller.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, Flask, redirect, render_template, request
+
+# import models and repos
+from models.Vet import Vet
+import repositories.vet_repository as vet_repo
+import repositories.animal_repository as animal_repo
+import repositories.appointment_repository as appt_repo
+import repositories.owner_repository as owner_repo
+
+
+# set up blueprint
+# <class>_blueprint = Blueprint("<class>", __name__)
+
+# INDEX
+
+
+# NEW
+
+
+
+# CREATE
+
+
+
+# EDIT
+
+
+
+# UPDATE
+
+
+
+
+# DELETE
