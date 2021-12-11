582d3c111a7a1f16bf899e3bee3dc96ca86294d5
petermordaunt <peetfire@hotmail.com>
2021-12-11 14:00:15 +0000

    DB created and .sql run. Checked tables in postico

--- code/db/animal_magic.sql
+++ code/db/animal_magic.sql
@@ -3,31 +3,31 @@ DROP TABLE IF EXISTS vets;
 DROP TABLE IF EXISTS animals;
 DROP TABLE IF EXISTS owners;
 
-CREATE TABLE owners(
+CREATE TABLE owners (
     id SERIAL PRIMARY KEY,
     name VARCHAR,
     email VARCHAR,
     phone VARCHAR
-)
+);
 
-CREATE TABLE animals(
+CREATE TABLE animals ( 
     id SERIAL PRIMARY KEY,
     name VARCHAR,
     species VARCHAR,
     dob VARCHAR,
     owner_id INT REFERENCES owners(id) ON DELETE CASCADE
-)
+);
 
-CREATE TABLE vets(
+CREATE TABLE vets (
     id SERIAL PRIMARY KEY,
     name VARCHAR,
-    animal INT REFERENCES animals(id) ON DELETE CASCADE
-)
+    animal_id INT REFERENCES animals(id) ON DELETE CASCADE
+);
 
-CREATE TABLE appointments(
+CREATE TABLE appointments (
     id SERIAL PRIMARY KEY,
     note_text TEXT,
     appt_date VARCHAR,
     animal_id INT REFERENCES animals(id) ON DELETE CASCADE,
     vet_id INT REFERENCES vets(id) ON DELETE CASCADE
-)
+);
